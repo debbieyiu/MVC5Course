8c5345b898aefb4d65ea270559216401fb084e4d
Debbie Yiu <debbie.yiu.0219@gmail.com>
2016-10-16 15:52:42 +0800

    24 練習匯入資料庫中的 vw_ClientContribution 檢視表，並顯示資料在畫面上

--- MVC5Course/Controllers/EFController.cs
+++ MVC5Course/Controllers/EFController.cs
@@ -96,5 +96,12 @@ namespace MVC5Course.Controllers
 			db.SaveChanges();
 			return RedirectToAction("Index");
 		}
+
+		public ActionResult ClientContribution()
+		{
+			var data = db.vw_ClientContribution.Take(10);
+
+			return View(data);
+		}
     }
 }
--- MVC5Course/Controllers/ProductsController.cs
+++ MVC5Course/Controllers/ProductsController.cs
@@ -46,7 +46,7 @@ namespace MVC5Course.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ProductId,ProductName,Price,Active,Stock")] Product product)
+        public ActionResult Create([Bind(Include = "ProductId,ProductName,Price,Active,Stock,IsDeleted")] Product product)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace MVC5Course.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ProductId,ProductName,Price,Active,Stock")] Product product)
+        public ActionResult Edit([Bind(Include = "ProductId,ProductName,Price,Active,Stock,IsDeleted")] Product product)
         {
             if (ModelState.IsValid)
             {
