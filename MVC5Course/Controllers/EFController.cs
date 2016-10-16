7dcc7d4509bd678041700e1ca32bd8bb99beae5c
Debbie Yiu <debbie.yiu.0219@gmail.com>
2016-10-16 16:24:19 +0800

    25 練習對 Entity Framework 的資料存取程式進行效能調校

--- MVC5Course/Controllers/EFController.cs
+++ MVC5Course/Controllers/EFController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVC5Course.Models;
+using MVC5Course.Models.ViewModels;
 
 namespace MVC5Course.Controllers
 {
@@ -83,17 +84,24 @@ namespace MVC5Course.Controllers
 			return RedirectToAction("Index");
 		}
 
+		//public ActionResult Add20Percent()
+		//{
+		//	var data = db.Product.Where(p => p.ProductName.Contains("White"));
+		//	foreach (var item in data)
+		//	{
+		//		if (item.Price.HasValue)
+		//		{ 
+		//			item.Price *= 1.2m;
+		//		}
+		//	}
+		//	db.SaveChanges();
+		//	return RedirectToAction("Index");
+		//}
+
 		public ActionResult Add20Percent()
 		{
-			var data = db.Product.Where(p => p.ProductName.Contains("White"));
-			foreach (var item in data)
-			{
-				if (item.Price.HasValue)
-				{ 
-					item.Price *= 1.2m;
-				}
-			}
-			db.SaveChanges();
+			string str = "%White%";
+			db.Database.ExecuteSqlCommand("UPDATE dbo.Product SET Price=Price*1.2 WHERE ProductName LIKE @p0", str);
 			return RedirectToAction("Index");
 		}
 
@@ -101,6 +109,22 @@ namespace MVC5Course.Controllers
 		{
 			var data = db.vw_ClientContribution.Take(10);
 
+			return View(data);
+		}
+
+		public ActionResult ClientContribution2(string keyward = "Mary")
+		{
+			var data = db.Database.SqlQuery<ClientContributionViewModel>(@" 
+				SELECT
+					 c.ClientId,
+					 c.FirstName,
+					 c.LastName,
+					 (SELECT SUM(o.OrderTotal) 
+					  FROM [dbo].[Order] o 
+					  WHERE o.ClientId = c.ClientId) as OrderTotal
+				FROM 
+					[dbo].[Client] as c
+				WHERE c.FirstName LIKE @p0", "%" + keyward + "%");
 			return View(data);
 		}
     }
