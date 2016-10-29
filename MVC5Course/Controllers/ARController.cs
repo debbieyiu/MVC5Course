e02db7a1bbaa5db1e9b2ae09eda03621170fc519
Debbie Yiu <debbie.yiu.0219@gmail.com>
2016-10-29 13:43:12 +0800

    41 練習 JsonResult 的使用

--- MVC5Course/Controllers/ARController.cs
+++ MVC5Course/Controllers/ARController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace MVC5Course.Controllers
 {
-    public class ARController : Controller
+    public class ARController : BaseController
     {
         // GET: AR
         public ActionResult Index()
@@ -35,6 +35,14 @@ namespace MVC5Course.Controllers
 		{
 			var filePath = Server.MapPath("~/Content/ppap.jpg");
 			return File(filePath, "image/jpeg", "ppap.jpg");
+		}
+
+		public ActionResult JsonTest()
+		{
+			//停用延遲載入
+			db.Configuration.LazyLoadingEnabled = false;
+			var data = db.Product.OrderBy(p => p.ProductId).Take(10);
+			return Json(data, JsonRequestBehavior.AllowGet);
 		}
     }
 }
