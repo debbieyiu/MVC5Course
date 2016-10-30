68eb9b4a65aef76740776cfe7851c65680de72ef
Debbie Yiu <debbie.yiu.0219@gmail.com>
2016-10-30 15:57:16 +0800

    練習將 ClientLoginViewModel 特定欄位修改為用 Password 欄位方式輸出

--- MVC5Course/Models/ViewModels/ClientLoginViewModel.cs
+++ MVC5Course/Models/ViewModels/ClientLoginViewModel.cs
@@ -13,6 +13,7 @@ namespace MVC5Course.Models.ViewModels
 		[Required]
 		[StringLength(maximumLength: 10, ErrorMessage = "{0} 最大不可超過{1}個自元")]
 		[DisplayName("姓")]
+		[DataType(DataType.Password)]
 		public string LastName { get; set; }
 		[DisplayName("生日")]
 		[DataType(DataType.Date)]
